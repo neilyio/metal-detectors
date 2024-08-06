7ffc5d39ff4ed80c671a5451f51df0e2a6b2e9ce
Neil Hansen <neil.hansen.31@gmail.com>
2024-08-05 21:00:32 -0700

    init

---
+++ test/neilyio/metal_detectors_test.clj
@@ -0,0 +1,7 @@
+(ns neilyio.metal-detectors-test
+  (:require [clojure.test :refer :all]
+            [neilyio.metal-detectors :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
