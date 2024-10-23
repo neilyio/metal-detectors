ff22f130f1c57b4ee6fd88d8dd988f70092d13ca
Steven Chu <stevenc1@gmail.com>
2024-10-22 20:20:20 -0700

    Got it working in a very rudimentary way

--- src/neilyio/metal_detectors.clj
+++ src/neilyio/metal_detectors.clj
@@ -1,6 +1,7 @@
 (ns neilyio.metal-detectors
   (:require [clojure.data.json :as json]
-            [clojure.core.async :refer [chan go-loop <! >! put!]])
+            [clojure.core.async :refer [chan go-loop <! >! put!]]
+            [clojure.java.shell :refer [sh]])
   (:import [java.net DatagramSocket DatagramPacket InetAddress SocketException])
   (:gen-class))
 
@@ -29,7 +30,25 @@
 (defn packet-data [packet]
   (String. (.getData packet) 0 (.getLength packet)))
 
-(defn process-json-data [json-string]
+(defn run-vlc-subprocess [file display]
+  (let [{:keys [origin_x origin_y width height]} display
+        vlc-path "/Applications/VLC.app/Contents/MacOS/VLC"
+        args [vlc-path
+              file
+              (str "--video-x=" origin_x)
+              (str "--video-y=" origin_y)
+              (str "--width=" width)
+              (str "--height=" height)
+              "--video-on-top"
+              "--fullscreen"
+              "--no-video-deco"
+              "--key-intf-show=false"
+              "--play-and-exit"]]
+    ;; Start VLC as a subprocess
+    (doto (ProcessBuilder. args)
+      (.start))))
+
+(defn process-json-data [json-string, screen-locations]
   (try
     (let [data (json/read-str json-string)
           uuid (get data "uuid")
@@ -50,7 +69,13 @@
                 :tx-power tx-power})
 
         ;; Print the updated map to show the new values
-        (println "Updated UUID-data map:" @uuid-data-map)))
+        (println "Updated UUID-data map:" @uuid-data-map)
+
+        ;; NOTE (stevenchu): A few considerations:
+        ;; 1/ Should only call this conditional on the data received
+        ;; 2/ We should load up all the filepaths to the videos ahead of time
+        ;; 3/ We need locking on the screens
+        (run-vlc-subprocess "/Users/stevenchu/Downloads/tongue-singing-choir.mp4" (rand-nth screen-locations))))
     (catch Exception e
       (println "Error processing JSON:" (.getMessage e)))))
 
@@ -72,27 +97,37 @@
             (recur))))
       (println (str "Failed to create a socket on port " port)))))
 
-(defn start-processing []
+(defn start-processing [screen-locations]
   "Starts a core.async loop that processes messages from the channel."
   (go-loop []
     (let [json-string (<! udp-channel)]
       ;; Process the JSON data received from the channel
-      (process-json-data json-string))
+      (process-json-data json-string screen-locations))
     ;; Continue processing the next message
     (recur)))
+
+(defn read-screen-locations []
+  (let [{:keys [out exit err]} (sh "python" "./py-scripts/display_locations.py")]
+    (if (zero? exit)
+      (json/read-str out :key-fn keyword)
+      (throw (Exception. (str "Error running script: " err))))))
 
 ;; Fix the `run-server` function to handle arguments correctly
 (defn run-server
   "Runs a UDP server. The function expects a map with the key `:port`."
   [{:keys [port] :or {port 5000}}] ;; Default to port 5000 if not provided
   (println (str "Starting server on port " port))
-  ;; Start the listening and processing functions
-  (start-listening port)
-  (start-processing)
+
+  ;; FIXME (stevenchu): Is this really the best place to store this variable?
+  ;; It keeps getting passed through
+  (let [screen-locations (read-screen-locations)]
+    ;; Start the listening and processing functions
+    (start-listening port)
+    (start-processing screen-locations)
 
   ;; Block the main thread so it doesn't exit immediately
-  (println "Press Enter to stop the server.")
-  (read-line))
+    (println "Press Enter to stop the server.")
+    (read-line)))
 
 (defn -main
   "Main entry point to start the UDP server."
